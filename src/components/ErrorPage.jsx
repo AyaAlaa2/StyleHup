bfa1bb478e9177e625bac1a8001df3fbfdbf83c3
aya alaa <aya-28-10@hotmail.com>
2025-09-30 17:12:06 +0300

    push ErrorPage

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { IoSearchOutline } from "react-icons/io5";
+
+const ErrorPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-start px-[16px] h-[495px]">
+      <div className="pt-[16px] pb-[12px]">
+        <p className="text-[32px] font-bold leading-[40px] text-[#141414]">
+          Page Not Found
+        </p>
+      </div>
+      <div className="pt-[4px] pb-[12px] w-[80%] text-center">
+        <p className="text-[16px] font-normal leading-[24px] text-[#141414]">
+          The page you are looking for does not exist or has been moved. Please
+          check the URL or use the search bar below to find what you need.
+        </p>
+      </div>
+      <div className="py-[12px] w-[80%]">
+        <label className="input flex bg-[#F2F2F2] border-[#F2F2F2] items-center justify-center px-[16px]">
+          <IoSearchOutline className="text-[#757575] text-[24px]" />
+          <input
+            type="search"
+            placeholder="Search  for products"
+            className="w-full rounded text-[16px] py-[12px] px-[8px] focus:outline-none text-[#757575]"
+          />
+        </label>
+      </div>
+      <div className="flex items-center justify-center px-[12px] gap-[12px] pt-[12px]">
+        <button className="px-[48px] py-[9.5px] bg-black text-white rounded-lg font-bold text-[14px] leading-[21px]">
+          Go To Homepage
+        </button>
+        <button className="px-[48px] py-[9.5px] bg-[#F2F2F2] text-black rounded-lg font-bold text-[14px] leading-[21px]">
+          Browse Categories
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorPage;
