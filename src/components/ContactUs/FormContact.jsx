c4867d2f94069875fdef69912adfcfd8c680818b
aya alaa <aya-28-10@hotmail.com>
2025-10-01 14:14:22 +0300

    push contact page

---
+++ src/components/ContactUs/FormContact.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const FormSchema = z.object({
+  name: z.string().min(2, {
+    message: "Name must be at least 2 characters.",
+  }),
+  email: z.string().email({
+    message: "Email is not vaild",
+  }),
+  message: z.string().min(2, {
+    message: "Message must be at least 2 characters.",
+  }),
+});
+
+const FormContact = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: zodResolver(FormSchema), mode: "onBlur" });
+
+  return (
+    <form onSubmit={handleSubmit} className="w-full md:w-2/5 space-y-[16px]">
+      <div className="flex flex-col justify-center items-start gap-[8px] w-full">
+        <label htmlFor="Name">
+          <span className="text-[16px] font-medium text-[#141414]">Name</span>
+        </label>
+        <input
+          type="text"
+          id="Name"
+          className="w-full rounded border border-[#E0E0E0] focus:outline-none p-2"
+          placeholder="Your Name"
+          required
+          {...register("name")}
+        />
+        {errors?.name && (
+          <p className="text-[13px] text-red-400">{errors.name.message}</p>
+        )}
+      </div>
+      <div className="flex flex-col justify-center items-start gap-[8px]">
+        <label htmlFor="Email">
+          <span className="text-[16px] font-medium text-[#141414]">Email</span>
+        </label>
+        <input
+          type="text"
+          id="Email"
+          className="w-full rounded border border-[#E0E0E0] focus:outline-none p-2"
+          placeholder="Your Email"
+          required
+          {...register("email")}
+        />
+        {errors?.email && (
+          <p className="text-[13px] text-red-400">{errors.email.message}</p>
+        )}
+      </div>
+      <div className="flex flex-col justify-center items-start gap-[8px]">
+        <label htmlFor="Notes">
+          <span className="text-[16px] font-medium text-[#141414]">
+            Message
+          </span>
+        </label>
+        <textarea
+          id="Notes"
+          className="w-full resize-none rounded border border-[#E0E0E0] focus:outline-none"
+          rows="4"
+          required
+          {...register("message")}
+        ></textarea>
+        {errors?.message && (
+          <p className="text-[13px] text-red-400">{errors.message.message}</p>
+        )}
+      </div>
+      <button
+        type="submit"
+        onClick={handleSubmit}
+        className="px-[16px] py-[8px] bg-black text-white rounded-lg font-bold text-[14px] cursor-pointer"
+      >
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default FormContact;
---
+++ src/components/ContactUs/MapContact.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+const customIcon = L.icon({
+  iconUrl: "https://cdn-icons-png.flaticon.com/512/684/684908.png",
+  iconSize: [40, 40],
+});
+
+const MapContact = () => {
+  const randomPosition = [
+    31.95 + (Math.random() - 0.5) * 0.1,
+    35.91 + (Math.random() - 0.5) * 0.1,
+  ];
+  return (
+    <div className="w-full h-96 rounded-2xl overflow-hidden shadow overflow-hidden mt-[12px]">
+      <MapContainer
+        center={randomPosition}
+        zoom={8}
+        style={{ height: "100%", width: "100%" }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={randomPosition} icon={customIcon}></Marker>
+      </MapContainer>
+    </div>
+  );
+};
+
+export default MapContact;
